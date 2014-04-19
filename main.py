15fb4103b649f5f762b1a066d0e064221fd552c6
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-04-18 20:27:26 -0700

    Add some debug.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from local_settings import consumer_key, consumer_secret, access_token_key,\
         access_token_secret
 import twitter
 
+debug = True
+
 def main():
     api = twitter.Api(consumer_key=consumer_key,
         consumer_secret=consumer_secret,
@@ -10,7 +12,12 @@ def main():
         access_token_secret=access_token_secret
     )
 
-    #status = api.PostUpdate('Beware I live.')
+    markov_text = None
+
+    if debug is False:
+        status = api.PostUpdate('Beware I live.')
+    else:
+        print "Want to post: {}".format(markov_text)
 
 if __name__ == '__main__':
     main()
